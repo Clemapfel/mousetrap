493a9c8c1cae307affce97adbc9968e872b9aa22
Clemapfel <mail@clemens-cords.com>
2022-09-13 21:52:32 +0200

    detachable working

---
+++ include/revealer.hpp
@@ -0,0 +1,43 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 9/13/22 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+#include <include/widget.hpp>
+
+namespace mousetrap
+{
+    enum class TransitionType
+    {
+        NONE = GTK_REVEALER_TRANSITION_TYPE_NONE,
+        CROSSFADE = GTK_REVEALER_TRANSITION_TYPE_CROSSFADE,
+        SLIDE_LEFT_TO_RIGHT = GTK_REVEALER_TRANSITION_TYPE_SLIDE_RIGHT,
+        SLIDE_RIGHT_TO_LEFT = GTK_REVEALER_TRANSITION_TYPE_SLIDE_LEFT,
+        SLIDE_TOP_TO_BOTTOM = GTK_REVEALER_TRANSITION_TYPE_SLIDE_DOWN,
+        SLIDE_BOTTOM_TO_TOP = GTK_REVEALER_TRANSITION_TYPE_SLIDE_UP,
+        SWING_LEFT_TO_RIGHT = GTK_REVEALER_TRANSITION_TYPE_SWING_RIGHT,
+        SWING_RIGHT_TO_LEFT = GTK_REVEALER_TRANSITION_TYPE_SWING_LEFT,
+        SWING_TOP_TO_BOTTOM = GTK_REVEALER_TRANSITION_TYPE_SWING_DOWN,
+        SWING_BOTTOM_TO_TOP = GTK_REVEALER_TRANSITION_TYPE_SWING_UP,
+    };
+
+    class Revealer : public Widget
+    {
+        public:
+            Revealer(TransitionType = TransitionType::CROSSFADE);
+            operator GtkWidget*() override;
+
+            void set_child(Widget*);
+
+            void set_revealed(bool b);
+            bool get_revealed();
+            void set_transition_type(TransitionType);
+
+        private:
+            GtkRevealer* _native;
+    };
+}
+
+#include <src/revealer.inl>
---
+++ src/revealer.inl
@@ -0,0 +1,49 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 9/13/22 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+namespace mousetrap
+{
+    Revealer::Revealer(TransitionType type)
+    {
+        _native = GTK_REVEALER(gtk_revealer_new());
+        set_transition_type(type);
+    }
+
+    Revealer::operator GtkWidget*()
+    {
+        return GTK_WIDGET(_native);
+    }
+
+    void Revealer::set_child(Widget* widget)
+    {
+        gtk_revealer_set_child(_native, widget->operator GtkWidget*());
+    }
+
+    void Revealer::set_revealed(bool b)
+    {
+        gtk_revealer_set_reveal_child(_native, b);
+
+        // trigger parent container reorder
+        auto* child = gtk_revealer_get_child(_native);
+        auto h = gtk_widget_get_hexpand(child);
+        auto v = gtk_widget_get_vexpand(child);
+        gtk_widget_set_hexpand(child, not h);
+        gtk_widget_set_vexpand(child, not v);
+        gtk_widget_set_hexpand(child, h);
+        gtk_widget_set_vexpand(child, v);
+    }
+
+    bool Revealer::get_revealed()
+    {
+        gtk_revealer_get_reveal_child(_native);
+    }
+
+    void Revealer::set_transition_type(TransitionType type)
+    {
+        gtk_revealer_set_transition_type(_native, (GtkRevealerTransitionType) type);
+    }
+}
