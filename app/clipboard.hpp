7f82c11d26861a8abfff28119adad59087957b3d
clem <mail@clemens-cords.com>
2023-03-05 19:33:42 +0100

    clipboard working

--- app/clipboard.hpp
+++ app/clipboard.hpp
@@ -8,38 +8,54 @@
 #include <string>
 
 #include <include/image.hpp>
+#include <include/widget.hpp>
 
 namespace mousetrap
 {
-    class ContentFormats
-    {
-        public:
-            ContentFormats(GdkContentFormats*);
-
-            void from_string(const std::string&);
-            std::string to_string() const;
-
-            bool operator==(const ContentFormats& other);
-            bool operator!=(const ContentFormats& other);
-
-        private:
-            GdkContentFormats* _native;
-    };
-
     class Clipboard
     {
         public:
-            Clipboard(GdkClipboard*);
-            ContentFormats get_formats() const;
+            Clipboard(Widget*);
+            operator GdkClipboard*();
+
+            bool contains_string() const;
+            bool contains_image() const;
+
             bool is_local() const;
 
-            void set_text(const std::string&);
-            std::string read_text() const;
+            void set_string(const std::string&);
+
+            /// @param Function_t: void(Clipboard*, const std::string&, Data_t)
+            template<typename Function_t, typename Data_t>
+            void get_string(Function_t on_string_read, Data_t);
 
             void set_image(const Image&);
 
+            /// @param Function_t: void(Clipboard*, const Image&, Data_t)
+            template<typename Function_t, typename Data_t>
+            void get_image(Function_t on_image_red, Data_t);
+
+        protected:
+            Clipboard(GdkClipboard*);
+
         private:
             GdkClipboard* _native;
-            static void read_text_callback(GObject* clipboard, GAsyncResult* result, gpointer instance);
+
+            std::function<void(Clipboard*, const std::string&)> get_string_f;
+            static void get_string_callback_wrapper(GObject* clipboard, GAsyncResult* result, gpointer data);
     };
+}
+
+//
+
+namespace mousetrap
+{
+    template <typename Function_t, typename Data_t>
+    void Clipboard::get_string(Function_t f_in, Data_t data_in)
+    {
+        get_string_f = [f = f_in, data = data_in](Clipboard* instance, const std::string& str){
+            f(instance, str, data);
+        };
+        gdk_clipboard_read_text_async(_native, nullptr, Clipboard::get_string_callback_wrapper, (gpointer) this);
+    }
 }
--- app/src/clipboard.cpp
+++ app/src/clipboard.cpp
@@ -6,95 +6,45 @@
 
 namespace mousetrap
 {
-    ContentFormats::ContentFormats(GdkContentFormats* native)
-        : _native(native)
+    Clipboard::Clipboard(GdkClipboard* clipboard)
+        : _native(clipboard)
     {}
 
-    void ContentFormats::from_string(const std::string& in)
-    {
-        _native = gdk_content_formats_parse(in.c_str());
-    }
-
-    std::string ContentFormats::to_string() const
-    {
-        return std::string(gdk_content_formats_to_string(_native));
-    }
-
-    bool ContentFormats::operator==(const ContentFormats& other)
-    {
-        return gdk_content_formats_match(_native, other._native);
-    }
-
-    bool ContentFormats::operator!=(const ContentFormats& other)
-    {
-        return not (*this == other);
-    }
-
-    Clipboard::Clipboard(GdkClipboard* native)
-        : _native(native)
+    Clipboard::Clipboard(Widget* widget)
+        : Clipboard(gtk_widget_get_clipboard(widget->operator GtkWidget *()))
     {}
 
-    ContentFormats Clipboard::get_formats() const
+    Clipboard::operator GdkClipboard*()
     {
-        return ContentFormats(gdk_clipboard_get_formats(_native));
+        return _native;
     }
 
-    bool Clipboard::is_local() const
+    bool Clipboard::contains_string() const
     {
-        return gdk_clipboard_is_local(_native);
+        auto* formats = gdk_clipboard_get_formats(_native);
+        return gdk_content_formats_contain_gtype(formats, G_TYPE_STRING);
     }
 
-    void Clipboard::set_image(const Image& image)
+    void Clipboard::set_string(const std::string& str)
     {
-        auto* pixbuf = image.to_pixbuf();
-        auto* texture = gdk_texture_new_for_pixbuf(pixbuf);
-        gdk_clipboard_set_texture(_native, texture);
-        g_object_unref(pixbuf);
-        g_object_unref(texture);
+        gdk_clipboard_set_text(_native, str.c_str());
     }
 
-    void Clipboard::set_text(const std::string& text)
-    {
-        gdk_clipboard_set_text(_native, text.c_str());
-    }
-
-    void Clipboard::read_text_callback(GObject* clipboard, GAsyncResult* result, gpointer instance)
+    void Clipboard::get_string_callback_wrapper(GObject* clipboard, GAsyncResult* result, gpointer data)
     {
         GError* error = nullptr;
         auto* text = gdk_clipboard_read_text_finish(GDK_CLIPBOARD(clipboard), result, &error);
 
-        if (error == nullptr and text != nullptr)
+        if (error != nullptr)
         {
-            std::cout << "success" << std::endl;
-            //std::cout << text << std::endl;
-        }
-
-        //g_free(text);
-    }
-
-    static void
-    text_loaded_cb (GObject      *clipboard,
-    GAsyncResult *res,
-    gpointer      data)
-    {
-        GError *error = NULL;
-        char *text;
-
-        text = gdk_clipboard_read_text_finish (GDK_CLIPBOARD (clipboard), res, &error);
-        if (text == NULL)
-        {
-            g_print ("%s\n", error->message);
-            g_error_free (error);
+            std::cerr << "[ERROR] In Clipboard::get_string_callback: " << error->message << std::endl;
             return;
         }
 
-        std::cout << text << std::endl;
-        g_free (text);
-    }
+        Clipboard* instance = (Clipboard*) data;
+        auto str = std::string(text);
 
-
-    std::string Clipboard::read_text() const
-    {
-        gdk_clipboard_read_text_async(_native, nullptr, text_loaded_cb, (gpointer) nullptr);
+        if (instance->get_string_f != nullptr)
+            instance->get_string_f(instance, text);
     }
 }
