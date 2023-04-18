a0648da79e6058348cd89a05620f34db32cc8ed5
clem <mail@clemens-cords.com>
2023-04-18 18:16:29 +0200

    implementing cmake

---
+++ cmake/install-rules.cmake
@@ -0,0 +1,21 @@
+set(package mousetrap)
+
+include(GNUInstallDirs)
+
+install(
+    DIRECTORY include
+    INCLUDES DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+)
+
+install(
+    TARGETS mousetrap
+    EXPORT mousetrap
+    LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}
+)
+
+include(CMakePackageConfigHelpers)
+write_basic_package_version_file(
+    "${package}-config-version.cmake"
+    VERSION ${PACKAGE_VERSION}
+    COMPATIBILITY AnyNewerVersion
+)
