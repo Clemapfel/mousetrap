08c81fa0dada67ab4eada787a393154c693643ca
clem <mail@clemens-cords.com>
2023-07-27 01:50:16 +0200

    added clamp frame

---
+++ include/mousetrap/clamp_frame.hpp
@@ -0,0 +1,75 @@
+//
+// Copyright (c) Clemens Cords (mail@clemens-cords.com), created 7/27/23
+//
+
+#pragma once
+
+#include <mousetrap/widget.hpp>
+#include <mousetrap/orientation.hpp>
+
+namespace mousetrap
+{
+    #ifndef DOXYGEN
+    class ClampFrame;
+    namespace detail
+    {
+        using ClampFrameInternal = AdwClamp;
+        DEFINE_INTERNAL_MAPPING(ClampFrame);
+    }
+    #endif
+
+    /// @brief container widget enforces a maximum size
+    /// \signals
+    /// \widget_signals{ClampFrame}
+    class ClampFrame : public detail::notify_if_gtk_uninitialized,
+        public Widget,
+        HAS_SIGNAL(ClampFrame, realize),
+        HAS_SIGNAL(ClampFrame, unrealize),
+        HAS_SIGNAL(ClampFrame, destroy),
+        HAS_SIGNAL(ClampFrame, hide),
+        HAS_SIGNAL(ClampFrame, show),
+        HAS_SIGNAL(ClampFrame, map),
+        HAS_SIGNAL(ClampFrame, unmap)
+    {
+        public:
+            /// @brief construct
+            /// @param orientation clamps width if horizontal, height if vertical
+            ClampFrame(Orientation);
+
+            /// @brief construct from internal \internal
+            /// @param internal
+            ClampFrame(detail::ClampFrameInternal*);
+
+            /// @brief dtor
+            ~ClampFrame();
+
+            /// @copydoc SignalEmitter::get_internal
+            NativeObject get_internal() const override;
+
+            /// @brief set orientation
+            /// @param orientation
+            void set_orientation(Orientation);
+
+            /// @brief get orientation
+            /// @return orientation
+            Orientation get_orientation() const;
+
+            /// @brief set the maximum width if horizontal, height if vertical
+            /// @param size in px
+            void set_maximum_size(float);
+
+            /// @brief get maximum width if horizontal, height if vertical
+            /// @return size in px
+            float get_maximum_size() const;
+
+            /// @brief set child
+            /// @param widget
+            void set_child(const Widget& widget);
+
+            /// @brief remove child, equivalent to <tt>set_child(nullptr)</tt>
+            void remove_child();
+
+        private:
+            detail::ClampFrameInternal* _internal = nullptr;
+    };
+}
---
+++ src/clamp_frame.cpp
@@ -0,0 +1,75 @@
+//
+// Created by clem on 7/27/23.
+//
+
+#include <mousetrap/clamp_frame.hpp>
+
+namespace mousetrap
+{
+    ClampFrame::ClampFrame(Orientation orientation)
+        : Widget(adw_clamp_new()),
+          CTOR_SIGNAL(ClampFrame, realize),
+          CTOR_SIGNAL(ClampFrame, unrealize),
+          CTOR_SIGNAL(ClampFrame, destroy),
+          CTOR_SIGNAL(ClampFrame, hide),
+          CTOR_SIGNAL(ClampFrame, show),
+          CTOR_SIGNAL(ClampFrame, map),
+          CTOR_SIGNAL(ClampFrame, unmap)
+    {
+        gtk_orientable_set_orientation(GTK_ORIENTABLE(Widget::operator NativeWidget()), (GtkOrientation) orientation);
+        _internal = ADW_CLAMP(g_object_ref(Widget::operator NativeWidget()));
+    }
+
+    ClampFrame::ClampFrame(detail::ClampFrameInternal* internal)
+        : Widget(GTK_WIDGET(internal)),
+          CTOR_SIGNAL(ClampFrame, realize),
+          CTOR_SIGNAL(ClampFrame, unrealize),
+          CTOR_SIGNAL(ClampFrame, destroy),
+          CTOR_SIGNAL(ClampFrame, hide),
+          CTOR_SIGNAL(ClampFrame, show),
+          CTOR_SIGNAL(ClampFrame, map),
+          CTOR_SIGNAL(ClampFrame, unmap)
+    {
+        g_object_ref(_internal);
+    }
+
+    ClampFrame::~ClampFrame()
+    {
+        g_object_unref(_internal);
+    }
+
+    void ClampFrame::set_orientation(Orientation orientation)
+    {
+        gtk_orientable_set_orientation(GTK_ORIENTABLE(Widget::operator NativeWidget()), (GtkOrientation) orientation);
+    }
+
+    Orientation ClampFrame::get_orientation() const
+    {
+        return (Orientation) gtk_orientable_get_orientation(GTK_ORIENTABLE(Widget::operator NativeWidget()));
+    }
+
+    void ClampFrame::set_maximum_size(float px)
+    {
+        adw_clamp_set_maximum_size(_internal, px);
+        adw_clamp_set_tightening_threshold(_internal, px);
+    }
+
+    float ClampFrame::get_maximum_size() const
+    {
+        return adw_clamp_get_maximum_size(_internal);
+    }
+
+    void ClampFrame::set_child(const Widget& child)
+    {
+        const Widget* child_ptr = &child;
+        WARN_IF_SELF_INSERTION(ClampFrame::set_child, this, child_ptr);
+        WARN_IF_PARENT_EXISTS(ClampFrame::set_child, child);
+
+        adw_clamp_set_child(_internal, child.operator NativeWidget());
+    }
+
+    void ClampFrame::remove_child()
+    {
+        adw_clamp_set_child(_internal, nullptr);
+    }
+}
