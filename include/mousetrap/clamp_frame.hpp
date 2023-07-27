cb19a569ce3fb8a74fe7c1205a20a78b70d66c77
clem <mail@clemens-cords.com>
2023-07-27 02:15:50 +0200

    implemente clamp frame

--- include/mousetrap/clamp_frame.hpp
+++ include/mousetrap/clamp_frame.hpp
@@ -30,6 +30,7 @@ namespace mousetrap
         HAS_SIGNAL(ClampFrame, show),
         HAS_SIGNAL(ClampFrame, map),
         HAS_SIGNAL(ClampFrame, unmap)
+
     {
         public:
             /// @brief construct
